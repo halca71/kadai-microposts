d50647b86a40fd4e7c0d6beb7057f6a924186d79
haruka.yoshida <mindmapping71@gmail.com>
2021-12-16 13:31:54 +0000

    add favorite

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -17,7 +17,7 @@ class MicropostsController < ApplicationController
   def destroy
     @micropost.destroy
     flash[:success] = 'メッセージを削除しました。'
-    redirect  _back(fallback_location: root_path)
+    redirect_back(fallback_location: root_path)
   end
   
   private
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,8 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  
+
+  has_many :favorites
+  has_many :likes, through: :favorites, source: :micropost
+
   validates :content, presence: true, length: { maximum: 255 }
 end
